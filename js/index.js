1fcf85dcc3ed27c7ed2fb279d4b7a85045b77d58
seml404 <4040591@gmail.com>
2022-02-22 21:26:05 +0300

    init

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(".menu li ul").hide();
+
+  $("li li:has(li)").find("a:first").addClass("arrow");
+
+  $("li").hover(
+    function () {
+      $(this).addClass("active");
+
+      $("ul:first", this).fadeIn();
+    },
+
+    function () {
+      $(this).removeClass("active");
+
+      $("ul:first", this).fadeOut();
+    }
+  );
+});
