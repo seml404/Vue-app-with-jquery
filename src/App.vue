8eeb6e5e0428565c1600ae82410b823dbafc1335
seml404 <4040591@gmail.com>
2022-02-22 22:03:04 +0300

    general second commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <p>it's value from vue: {{ value }}</p>
+    <ul class="menu">
+      <li><a href="#">Title1</a></li>
+      <li>
+        <a href="#">Title2</a>
+        <ul>
+          <li><a href="#">subtitle</a></li>
+          <li><a href="#">subtitle</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">Title2</a>
+        <ul>
+          <li>
+            <a href="#">subtitle</a>
+            <ul>
+              <li><a href="#">sub-subtitle</a></li>
+              <li><a href="#">sub-subtitle</a></li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import { jqLogic } from "./js";
+import $ from "jquery";
+import { ref } from "vue";
 export default {
+  setup() {
+    const value = ref("hello");
+    return {
+      value,
+    };
+  },
   name: "App",
-  components: {
-    HelloWorld,
+  components: {},
+  mounted() {
+    jqLogic();
+    $("li").hover(() => (this.value = "hovered"));
+    $("li").mouseleave(() => (this.value = "default"));
   },
 };
 </script>
@@ -22,5 +55,49 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  margin: 0;
+  padding: 0;
+}
+.menu li {
+  float: left;
+  position: relative;
+  border: 1px solid #5588c2;
+  margin-left: -1px;
+  list-style: none;
+}
+.menu a {
+  color: #5588c2;
+  font: 12px Verdana;
+  text-decoration: none;
+  display: block;
+  padding: 5px;
+  outline: none;
+}
+
+.menu li ul {
+  position: absolute;
+  width: 150px;
+  margin: 0 0 0 -1px;
+  background: #fff;
+  border: 1px solid #5588c2;
+  padding: 0;
+}
+
+.menu li li {
+  border: none;
+  width: 150px;
+  margin: 0;
+}
+.menu li li ul {
+  width: 150px;
+  margin: -1px 0 0 150px;
+  top: 0px;
+}
+
+.active {
+  background: #dadada;
 }
 </style>
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,16 @@
-$(document).ready(function () {
+import $ from "jquery";
+
+export function jqLogic() {
   $(".menu li ul").hide();
-
   $("li li:has(li)").find("a:first").addClass("arrow");
-
   $("li").hover(
     function () {
       $(this).addClass("active");
-
       $("ul:first", this).fadeIn();
     },
-
     function () {
       $(this).removeClass("active");
-
       $("ul:first", this).fadeOut();
     }
   );
-});
+}
